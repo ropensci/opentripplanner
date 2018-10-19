07af2001ac56b62b129797ac8833b2cbf03b2594
Layik Hama <layik.hama@gmail.com>
2018-10-19 12:24:19 +0100

    Stop example running

--- R/otp-connect.R
+++ R/otp-connect.R
@@ -12,7 +12,7 @@
 #'     instance and router are confirmed reachable. Optional, default is TRUE.
 #' @return Returns an S3 object of class otpconnect. If \code{check} is TRUE
 #'     and the router is not reachable the object is not returned.
-#' @examples
+#' @examples \dontrun{
 #' otpcon <- otp_connect()
 #' otpcon <- otp_connect(router = "UK2018",
 #'                       ssl = TRUE)
@@ -20,7 +20,7 @@
 #'                       router = "UK2018",
 #'                       port = 8888,
 #'                       ssl = TRUE)
-
+#' }
 #' @export
 otp_connect <- function(hostname = "localhost",
                         router = "default",
