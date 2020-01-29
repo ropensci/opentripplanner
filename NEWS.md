4800c35b1e218858ad7ee6b766adff73979e71a9
mem48 <>
2020-01-29 12:48:29 +0000

    Fix bug with fares

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# opentripplanner 0.2.0.6
+
+* Fixed bug where routing failes due to missing fare data
+
 # opentripplanner 0.2.0.4
 
 * Added a `NEWS.md` file to track changes to the package.
--- R/otp-plan.R
+++ R/otp-plan.R
@@ -555,11 +555,17 @@ otp_json2sf <- function(obj, full_elevation = FALSE, get_geometry = TRUE) {
 
   itineraries$legs <- NULL
 
-  # Extract Fare Info and discard for now
+  # Extract Fare Info
   fare <- itineraries$fare
   if (!is.null(fare)) {
-    itineraries$fare <- fare$fare$regular$cents / 100
-    itineraries$fare_currency <- fare$fare$regular$currency$currency
+    if(ncol(fare$fare) > 0){
+      itineraries$fare <- fare$fare$regular$cents / 100
+      itineraries$fare_currency <- fare$fare$regular$currency$currency
+    } else {
+      warning("Unstructured fare data has been discarded")
+      itineraries$fare <- NA
+      itineraries$fare_currency <- NA
+    }
   } else {
     itineraries$fare <- NA
     itineraries$fare_currency <- NA
